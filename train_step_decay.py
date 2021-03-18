c6b2b6c0486386381ba972c8ba13e5ac1a6cc447
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-19 00:18:29 +0300

    Update train_step_decay.py

--- train_step_decay.py
+++ train_step_decay.py
@@ -66,7 +66,7 @@ def build_model():
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 def step_decay(epoch):
    initial_lrate = 0.1
-   drop = 0.2
+   drop = 0.35
    epochs_drop = 5.0
    lrate = initial_lrate * math.pow(drop,  
            math.floor((1+epoch)/epochs_drop))
